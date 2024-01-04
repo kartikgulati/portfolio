3a47798d6cd238526ec9ac349f672c9e9ed45af5
Kartik Gulati <kartikgulati19@gmail.com>
2024-01-04 00:03:41 -0500

    commint changes

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,29 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data=[
+    {
+        id:1,
+        image: Image1,
+        name:"kartik gulati1",
+        title:"titile",
+        description:"description",
+
+    },{
+
+        id:1,
+        image: Image2,
+        name:"kartik gulati2",
+        title:"titile",
+        description:"description",
+
+    },
+    {
+        id:1,
+        image: Image3,
+        name:"kartik gulati3",
+        title:"titile",
+        description:"description",
+    }
+];
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <section className="testimonials comntainer sectiuon">
+         <h2 className="section_title">Testimonials Sec</h2>
+         <span className="section_subtitle">Here is the test section  </span>
+
+         <div className="testimonials_container"></div>
+
+
+         
+    </section>
+  )
+}
+
+export default Testimonials
